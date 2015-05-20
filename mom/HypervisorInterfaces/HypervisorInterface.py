f479bbc51592b94f2476a52a7dc826b7d1c38931
Francesco Romani <fromani@redhat.com>
2015-05-20 17:46:51 +0200

    hypervisor interface: inherit from object
    make sure the HypervisorInterface inherits from object.
    
    Change-Id: I7339176b953f056ff0a45a60ca23e7a5ff4c4e74
    Signed-off-by: Francesco Romani <fromani@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/41224
    Reviewed-by: Jenkins CI
    Reviewed-by: Adam Litke <alitke@redhat.com>

--- mom/HypervisorInterfaces/HypervisorInterface.py
+++ mom/HypervisorInterfaces/HypervisorInterface.py
@@ -14,7 +14,8 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
-class HypervisorInterface:
+
+class HypervisorInterface(object):
     """
     HypervisorInterface is an abstract class which defines all interfaces
     used by MOM to get guest memory statistics and control guest memory
