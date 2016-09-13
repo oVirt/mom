35c68977900da612b127ad0b0b11e45342e642fe
Jenny Tokar <jtokar@redhat.com>
2016-09-13 17:53:12 +0300

    Add caching for vm IO tune policies
    Instead of querying vdsm for each vm's IO tune policy seperatly
    this allows MOM to query vdsm once for all the needed info
    and cache the result for all the available vms.
    
    Change-Id: Ief6a0ee56e30c6e973321893da30f0b954cb3cfc
    Bug-Url: https://bugzilla.redhat.com/1366556
    Signed-off-by: Jenny Tokar <jtokar@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/63971
    Continuous-Integration: Jenkins CI
    Reviewed-by: Martin Sivák <msivak@redhat.com>

---
+++ mom/HypervisorInterfaces/vdsmjsonrpcbulkInterface.py
@@ -0,0 +1,48 @@
+# Memory Overcommitment Manager
+# Copyright (C) 2016 Martin Sivak, Red Hat
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+from .vdsmxmlrpcInterface import memoize
+
+from mom.HypervisorInterfaces.vdsmjsonrpcInterface import JsonRpcVdsmInterface, \
+    CACHE_EXPIRATION
+
+class JsonRpcVdsmBulkInterface(JsonRpcVdsmInterface):
+    """
+    JsonRpcVdsmBulkInterface extends the JsonRpcVdsmInterface and
+    overrides the getIoTune and getIoTunePolicy methods so that
+    the new vdsm api can be utilized
+    """
+    def __init__(self):
+        super(JsonRpcVdsmBulkInterface, self).__init__()
+
+    @memoize(expiration=CACHE_EXPIRATION)
+    def getAllVmIoTunePolicies(self):
+        ret = self.checked_call(self._vdsm_api.getAllVmIoTunePolicies)
+        return ret
+
+    def getVmIoTunePolicy(self, vmId):
+        vm_io_tune_policy_info = self.getAllVmIoTunePolicies()
+        result = vm_io_tune_policy_info[vmId]['policy']
+        return result.orNone()
+
+    def getVmIoTune(self, vmId):
+        vm_io_tune_policy_info = self.getAllVmIoTunePolicies()
+        result = vm_io_tune_policy_info[vmId]['current_values']
+        return result.orNone()
+
+def instance(config):
+    return JsonRpcVdsmBulkInterface()
+
---
+++ mom/HypervisorInterfaces/vdsmxmlrpcbulkInterface.py
@@ -0,0 +1,60 @@
+# Memory Overcommitment Manager
+# Copyright (C) 2015 Martin Sivak, Red Hat
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+from .vdsmxmlrpcInterface import memoize
+
+from mom.HypervisorInterfaces.vdsmxmlrpcInterface import XmlRpcVdsmInterface, \
+    CACHE_EXPIRATION
+
+from mom.optional import Optional
+
+class XmlRpcVdsmBulkInterface(XmlRpcVdsmInterface):
+    """
+    XmlRpcVdsmBulkInterface extends the XmlRpcVdsmInterface and
+    overrides the getIoTune and getIoTunePolicy methods so that
+    the new vdsm api can be utilized
+    """
+
+    def __init__(self):
+        super(XmlRpcVdsmBulkInterface, self).__init__()
+
+    @memoize(expiration=CACHE_EXPIRATION)
+    def getAllVmIoTunePolicies(self):
+        try:
+            ret = self.vdsm_api.getAllVmIoTunePolicies()
+            self._check_status(ret)
+        except socket.error as e:
+            self.handle_connection_error(e)
+            return Optional.empty()
+        except vdsmException as e:
+            e.handle_exception()
+            return Optional.empty()
+
+        vms = ret['io_tune_policies_dict']
+        return Optional(vms)
+
+    def getVmIoTunePolicy(self, vmId):
+        vm_io_tune_policy_info = self.getAllVmIoTunePolicies()
+        result = vm_io_tune_policy_info[vmId]['policy']
+        return result.orNone()
+
+    def getVmIoTune(self, vmId):
+        vm_io_tune_policy_info = self.getAllVmIoTunePolicies()
+        result = vm_io_tune_policy_info[vmId]['current_values']
+        return result.orNone()
+
+def instance(config):
+    return XmlRpcVdsmBulkInterface()
