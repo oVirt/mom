2ebf5eb09451b04420d7d3d263a9d5ac5c9f38f6
Sandro Bonazzola <sbonazzo@redhat.com>
2019-05-10 17:35:27 +0200

    automation: add install test
    verify all dependencies are satisfied when installing rpms.
    
    Change-Id: Ibac1098f8e9b0479dcb5b95a03b0b2b04ad929d8
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/99906
    Continuous-Integration: Jenkins CI <jenkins@ovirt.org>
    Reviewed-by: Andrej Krejcir <akrejcir@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -xe
 automation/build-artifacts.sh
+
+DISTVER="$(rpm --eval "%dist"|cut -c2-3)"
+PACKAGER=""
+if [[ "${DISTVER}" == "el" ]]; then
+    PACKAGER=yum
+else
+    PACKAGER=dnf
+fi
+
+${PACKAGER} --downloadonly install exported-artifacts/*noarch*.rpm
