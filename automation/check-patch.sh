2732045158037a9b5613e9e7cc3310a980db5787
Sandro Bonazzola <sbonazzo@redhat.com>
2019-05-13 12:02:04 +0200

    automation: run tests with coverage
    Change-Id: I903f87d5b88434827ba80beaed1b9699fd882092
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/99950
    Continuous-Integration: Jenkins CI <jenkins@ovirt.org>
    Tested-by: Gal Zaidman <gzaidman@redhat.com>
    Reviewed-by: Andrej Krejcir <akrejcir@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -5,8 +5,25 @@ DISTVER="$(rpm --eval "%dist"|cut -c2-3)"
 PACKAGER=""
 if [[ "${DISTVER}" == "el" ]]; then
     PACKAGER=yum
+    cd tests
+    PYTHONDONTWRITEBYTECODE=1 \
+	LC_ALL=C \
+	PYTHONPATH="..:./:$PYTHONPATH" \
+	python2 -m coverage run --rcfile=../automation/coverage.rc testrunner.py *.py
+    coverage html --rcfile=../automation/coverage.rc
+    mv htmlcov ../exported-artifacts/htmlcov-py2
+    cd ..
 else
     PACKAGER=dnf
+    cd tests
+    rm -f .coverage
+    PYTHONDONTWRITEBYTECODE=1 \
+	LC_ALL=C \
+	PYTHONPATH="..:.:$PYTHONPATH" \
+	python3 -m coverage run --rcfile=../automation/coverage.rc testrunner.py *.py
+    coverage3 html --rcfile=../automation/coverage.rc
+    mv htmlcov ../exported-artifacts/htmlcov-py3
+    cd ..
 fi
 
 ${PACKAGER} --downloadonly install exported-artifacts/*noarch*.rpm
