11cda3c1c83d6f950512cddd2246c54a0c53bafa
Andrej Krejcir <akrejcir@redhat.com>
2019-07-15 16:30:31 +0200

    automation: Add build date and git hash to snapshot builds
    Change-Id: I4273130cef6996be5b25e85994d1d15d99aae353
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>
    Reviewed-on: https://gerrit.ovirt.org/101813
    Continuous-Integration: Jenkins CI <jenkins@ovirt.org>
    Reviewed-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -2,17 +2,27 @@
 [[ -d exported-artifacts ]] \
 || mkdir -p exported-artifacts
 
-[[ -d rpmbuild/SOURCES ]] \
-|| mkdir -p rpmbuild/SOURCES
+BUILD_DIR="$PWD/rpmbuild"
+
+[[ -d "$BUILD_DIR/SOURCES" ]] \
+|| mkdir -p "$BUILD_DIR/SOURCES"
 
 ./autogen.sh
 ./configure --prefix=/usr
 make dist
-rpmbuild -ta *.tar.gz
+
+SUFFIX=
+. automation/config.sh
+[ -n "${VERSION_SUFFIX}" ] && SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
+
+rpmbuild \
+    -D "_topdir $BUILD_DIR" \
+    ${SUFFIX:+-D "release_suffix ${SUFFIX}"} \
+    -ta *.tar.gz
 
 # Move tarball to exported artifacts
 mv *.tar.gz exported-artifacts/
 
 # Move RPMs to exported artifacts
-find rpmbuild -iname \*rpm -exec mv "{}" exported-artifacts/ \;
+find "$BUILD_DIR" -iname \*rpm -exec mv "{}" exported-artifacts/ \;
 
