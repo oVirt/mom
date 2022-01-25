fd33bd61aa6e0f85ada97162801df59f68f854dc
Tomáš Golembiovský <tgolembi@redhat.com>
2022-01-25 11:56:33 +0100

    automation: add GitHub action
    Signed-off-by: Tomáš Golembiovský <tgolembi@redhat.com>
    Change-Id: I45ab2776e53dcd5278ef478e15b32bc49cf49743

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,29 +1,14 @@
 #!/bin/bash -xe
 automation/build-artifacts.sh
 
-DISTVER="$(rpm --eval "%dist"|cut -c2-4)"
-PACKAGER=""
-if [[ "${DISTVER}" == "el7" ]]; then
-    PACKAGER=yum
-    cd tests
-    PYTHONDONTWRITEBYTECODE=1 \
-	LC_ALL=C \
-	PYTHONPATH="..:./:$PYTHONPATH" \
-	python2 -m coverage run --rcfile=../automation/coverage.rc testrunner.py ./*.py
-    coverage html --rcfile=../automation/coverage.rc
-    mv htmlcov ../exported-artifacts/htmlcov-py2
-    cd ..
-else
-    PACKAGER=dnf
-    cd tests
-    rm -f .coverage
-    PYTHONDONTWRITEBYTECODE=1 \
-	LC_ALL=C \
-	PYTHONPATH="..:.:$PYTHONPATH" \
-	python3 -m coverage run --rcfile=../automation/coverage.rc testrunner.py ./*.py
-    coverage3 html --rcfile=../automation/coverage.rc
-    mv htmlcov ../exported-artifacts/htmlcov-py3
-    cd ..
-fi
+cd tests
+rm -f .coverage
+PYTHONDONTWRITEBYTECODE=1 \
+    LC_ALL=C \
+    PYTHONPATH="..:.:$PYTHONPATH" \
+    python3 -m coverage run --rcfile=../automation/coverage.rc testrunner.py ./*.py
+python3 -m coverage html --rcfile=../automation/coverage.rc
+mv htmlcov ../exported-artifacts/htmlcov-py3
+cd ..
 
-${PACKAGER} --downloadonly install exported-artifacts/*noarch*.rpm
+dnf --downloadonly install -y exported-artifacts/*noarch*.rpm
